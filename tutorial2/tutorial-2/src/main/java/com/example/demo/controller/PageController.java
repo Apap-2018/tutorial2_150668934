167e0cffb41136b1b40a20a05251180f540f88a6
ratnanurhayati <ratnanana07@gmail.com>
2018-09-22 21:30:17 +0700

    upload tutorial 2

---
+++ tutorial2/tutorial-2/src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,57 @@
+package com.example.demo.controller;
+
+import java.util.Optional;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PageController {
+    @RequestMapping("/viral")
+    public String index(){
+        return "viral";
+    }
+    
+    @RequestMapping("/challenge")
+    public String challenge(@RequestParam(value = "name") String name, Model model) {
+    	model.addAttribute("name", name);
+    	return "challenge";
+    }
+    
+    
+    @RequestMapping(value = {"/challenge", "/challenge/{name}"})
+    public String challengePath(@PathVariable Optional<String> name, Model model) {
+    	if (name.isPresent()) {
+    		model.addAttribute("name", name.get());
+    	} else {
+    		model.addAttribute("name", "KB");
+    	}
+    	return "challenge";
+    }
+    
+    @RequestMapping("/generator")
+    public String viralGenerator(@RequestParam(value = "a", required=false, defaultValue="0") int a,
+    							@RequestParam(value = "b", required=false, defaultValue="0") int b, Model model) {
+    	
+    	String result = "hm";
+    	
+    	for(int i = 1; i < a; i++) {
+    		result = result + "m";
+    	}
+    	
+    	
+    	for(int j = 1; j < b; j++) {
+    		result = result + " " + result;
+    	}
+    	
+    	
+    	
+    	model.addAttribute("a", a);
+    	model.addAttribute("b", b);
+    	model.addAttribute("result", result);
+    	
+    	return "generator";
+    }
+}
